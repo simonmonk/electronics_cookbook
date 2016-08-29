a1e5871e30503b5ae53d524ca94fbf404d71831e
simonmonk <srmonk@gmail.com>
2016-08-29 11:11:55 +0000

    rangefinder improvements

--- pi/ch_12_rangefinder.py
+++ pi/ch_12_rangefinder.py
@@ -2,29 +2,27 @@ import RPi.GPIO as GPIO
 import time
 
 trigger_pin = 18
-echo_pin = 23
+echo_pin = 23 # USE LEVEL CONVERTER 5V->3.3V
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(trigger_pin, GPIO.OUT)
 GPIO.setup(echo_pin, GPIO.IN)
 
-def send_trigger_pulse():
-    GPIO.output(trigger_pin, True)
-    time.sleep(0.0001)
-    GPIO.output(trigger_pin, False)
-
-def wait_for_echo(value, timeout):
-    count = timeout
-    while GPIO.input(echo_pin) != value and count > 0:
-        count = count - 1
+def time_to_echo(timeout):
+    t0 = time.time()
+    while GPIO.input(echo_pin) == False and time.time() < (t0 + timeout):
+        pass
+    t0 = time.time()
+    while GPIO.input(echo_pin) == True and time.time() < (t0 + timeout):
+        pass
+    return time.time() - t0
 
 def get_distance():
-    send_trigger_pulse()
-    wait_for_echo(True, 10000)
-    start = time.time()
-    wait_for_echo(False, 10000)
-    finish = time.time()
-    pulse_len = finish - start
+    GPIO.output(trigger_pin, True)
+    time.sleep(0.00001)     # 10us
+    GPIO.output(trigger_pin, False)
+    time.sleep(0.0002)      # 200us
+    pulse_len = time_to_echo(1)
     distance_cm = pulse_len / 0.000058
     distance_in = distance_cm / 2.5
     return (distance_cm, distance_in)
