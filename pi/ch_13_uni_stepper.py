13751569f32cc877a8bf39467bcf4dcf69e36877
simonmonk <srmonk@gmail.com>
2016-08-31 09:30:22 +0000

    stepper pi

--- pi/ch_13_uni_stepper.py
+++ pi/ch_13_uni_stepper.py
@@ -3,10 +3,10 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-in_1_pin = 23     # (1)
-in_2_pin = 24
-in_3_pin = 25
-in_4_pin = 18
+in_1_pin = 18
+in_2_pin = 23
+in_3_pin = 24
+in_4_pin = 25
 
 GPIO.setup(in_1_pin, GPIO.OUT)
 GPIO.setup(in_2_pin, GPIO.OUT)
@@ -15,7 +15,7 @@ GPIO.setup(in_4_pin, GPIO.OUT)
 
 period = 0.02
 
-def step_forward(steps, period):    # (2)
+def step_forward(steps, period):  
   for i in range(0, steps):
     set_coils(1, 0, 0, 1)
     time.sleep(period)
@@ -26,7 +26,7 @@ def step_forward(steps, period):    # (2)
     set_coils(0, 1, 0, 1)
     time.sleep(period)
 
-def step_reverse(steps, period):   
+def step_reverse(steps, period):  
   for i in range(0, steps):
     set_coils(0, 1, 0, 1)
     time.sleep(period)
@@ -38,7 +38,7 @@ def step_reverse(steps, period):
     time.sleep(period)
 
   
-def set_coils(in1, in2, in3, in4):     # (3)
+def set_coils(in1, in2, in3, in4):
   GPIO.output(in_1_pin, in1)
   GPIO.output(in_2_pin, in2)
   GPIO.output(in_3_pin, in3)
@@ -51,11 +51,11 @@ try:
     print('f100 - forward 100 steps');
     print('r100 - reverse 100 steps');
     
-    while True:      # (4)
+    while True:
         command = raw_input('Enter command: ')
         parameter_str = command[1:] # from char 1 to end
-        parameter = int(parameter_str)     # (5)
-        if command[0] == 'p':     # (6)
+        parameter = int(parameter_str)
+        if command[0] == 'p':
             period = parameter / 1000.0
         elif command[0] == 'f':
             step_forward(parameter, period)
