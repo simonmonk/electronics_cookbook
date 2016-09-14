93b24e7b253313c11119d8d741f583e7cfb10703
simonmonk <srmonk@gmail.com>
2016-09-14 07:58:15 +0000

    added stuff

--- pi/ch_15_decade_counter.py
+++ pi/ch_15_decade_counter.py
@@ -9,7 +9,7 @@ clock_pin = 23
 GPIO.setup(reset_pin, GPIO.OUT)
 GPIO.setup(clock_pin, GPIO.OUT)
 
-def set_ditit(digit): 
+def set_digit(digit): 
     GPIO.output(reset_pin, True)
     time.sleep(0.000001)
     GPIO.output(reset_pin, False)
@@ -23,7 +23,7 @@ def set_ditit(digit):
 try:         
     while True:
         digit = input("Enter digit 0..9 ")
-        set_ditit(digit)
+        set_digit(digit)
         
 finally:  
     print("Cleaning up")
